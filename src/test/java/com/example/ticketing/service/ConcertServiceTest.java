f88a3e2f54e8962381b0969de2a8f01d0eb0f052
b_loso <jaesme2530@gmail.com>
2024-06-27 01:08:22 +0900

    [TEST] 콘서트 모두조회 테스트 추가

--- src/test/java/com/example/ticketing/service/ConcertServiceTest.java
+++ src/test/java/com/example/ticketing/service/ConcertServiceTest.java
@@ -1,20 +1,30 @@
 package com.example.ticketing.service;
 
+import com.example.ticketing.controller.dto.ConcertDTO;
 import com.example.ticketing.service.domain.Concert;
+import com.example.ticketing.service.mapper.ConcertMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
+// Mockito 사용시, MockitoAnnotations.openMocks(this) 호출해서 Mockito 어노테이션 초기화 필요 
+// or @ExtendWith(MockitoExtension.class) 사용 class 레벨
+@ExtendWith(MockitoExtension.class)
 class ConcertServiceTest {
 
     @Mock
@@ -22,18 +32,27 @@ class ConcertServiceTest {
     @InjectMocks
     private ConcertService concertService;
 
+//    @BeforeEach
+//    void setUp() {
+//        MockitoAnnotations.openMocks(this);
+//    }
+
     @Test
     void getConcertList() {
         // given
-        List<Concert> expectedConcertList = new ArrayList<>();
-        expectedConcertList.add(new Concert(1L, "콘서트1", "장소1", LocalDate.now(), LocalTime.now(), 100, 10000, LocalDateTime.now(), LocalDateTime.now()));
-        expectedConcertList.add(new Concert(2L, "콘서트2", "장소2", LocalDate.now(), LocalTime.now(), 200, 20000, LocalDateTime.now(), LocalDateTime.now()));
-        when(concertRepository.findAll()).thenReturn(expectedConcertList);
+        List<Concert> concertList = new ArrayList<>();
+        concertList.add(new Concert(1L, "콘서트1", "장소1", LocalDate.now(), LocalTime.now(), 100, 10000, LocalDateTime.now(), LocalDateTime.now()));
+        concertList.add(new Concert(2L, "콘서트2", "장소2", LocalDate.now(), LocalTime.now(), 200, 20000, LocalDateTime.now(), LocalDateTime.now()));
+        when(concertRepository.findAll()).thenReturn(concertList);
+
+        List<ConcertDTO> expectedConcertDTOList = concertList.stream()
+            .map(ConcertMapper::toDTO)
+            .collect(Collectors.toList());
 
         // when
-        List<Concert> actualConcertList = concertService.getConcertList();
+        List<ConcertDTO> actualConcertList = concertService.getConcertList();
 
         // then
-        assertThat(actualConcertList).isEqualTo(expectedConcertList);
+        assertThat(actualConcertList).isEqualTo(expectedConcertDTOList);
     }
 }
